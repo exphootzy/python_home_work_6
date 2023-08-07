0a31444aa0ab9c23dd7c02ccabd4724c6c942f5d
exphootzy <enjoyphoto@mail.ru>
2023-08-07 21:39:46 +0900

    2nd task completed

--- task_32.py
+++ task_32.py
@@ -1,3 +1,17 @@
 # Определить индексы элементов массива (списка),
 # значения которых принадлежат заданному диапазону (т.е.
 # не меньше заданного минимума и не больше заданного максимума)
+import random 
+
+n=(int(input("Введите число для n элементов для первого множества: ")))
+list_one = [random.randint(-10, 11) for x in range(1, n+1)]
+print(list_one)
+
+list_total = []
+min_value = 6
+max_value = 10
+
+for i in range(len(list_one)):
+    if list_one[i] >= min_value and list_one[i] <= max_value:
+        list_total.append(i)
+print(list_total)
